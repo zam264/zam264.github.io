72f2160ea3456c6d573a260c233333d3f9f8902c
Steven Zamborsky <zam264@users.noreply.github.com>
2017-11-19 12:12:35 -0500

    initial commit

---
+++ src/fizzbuzz.js
@@ -0,0 +1,16 @@
+function fizzBuzz(){                
+    for(var i = 1; i <= 100; i++){
+        if(i % 15 == 0){
+            document.getElementById("fizzBuzz").innerHTML += (i + " FizzBuzz" + "<br>");
+        }
+        else if(i % 3 == 0){
+            document.getElementById("fizzBuzz").innerHTML += (i + " Fizz" + "<br>");
+        }
+        else if(i % 5 == 0){
+            document.getElementById("fizzBuzz").innerHTML += (i + " Buzz" + "<br>");
+        }
+        else{
+            document.getElementById("fizzBuzz").innerHTML += (i) + "<br>";
+        }                    
+    }              
+}
---
+++ src/main.js
@@ -0,0 +1,14 @@
+function genNavBar(){
+    var navBarItems = [
+        ["index.html", "Home"],
+        ["about.html", "About"],
+        ["resume.html", "Resume"],
+        ["fizzbuzz.html", "FizzBuzz"],
+        ["template.html", "Template"]
+    ]
+    var numNavBarItems = navBarItems.length;
+    document.getElementById("navTable").innerHTML += "<tr id='navBar'></tr>";
+    for(var i = 0; i < numNavBarItems; i++){
+        document.getElementById("navBar").innerHTML += "<td>" + "<a href=" + navBarItems[i][0] + ">" + navBarItems[i][1] + "</a></td>";        
+    }
+}
