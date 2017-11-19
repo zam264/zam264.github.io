d3a22317c75b29d1781b5c6182745cd1b3d7187e
Steven Zamborsky <zam264@users.noreply.github.com>
2017-11-19 12:29:59 -0500

    restructured site to work with GitHub Pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-function genNavBar(){
+function genNavBar() {
     var navBarItems = [
         ["index.html", "Home"],
         ["about.html", "About"],
@@ -8,7 +8,7 @@ function genNavBar(){
     ]
     var numNavBarItems = navBarItems.length;
     document.getElementById("navTable").innerHTML += "<tr id='navBar'></tr>";
-    for(var i = 0; i < numNavBarItems; i++){
-        document.getElementById("navBar").innerHTML += "<td>" + "<a href=" + navBarItems[i][0] + ">" + navBarItems[i][1] + "</a></td>";        
+    for (var i = 0; i < numNavBarItems; i++) {
+        document.getElementById("navBar").innerHTML += "<td>" + "<a href=" + navBarItems[i][0] + ">" + navBarItems[i][1] + "</a></td>";
     }
 }
