a37422c5292ba8a68d1ddd9ffc575fc3c90f8e1d
Steven Zamborsky <zam264@gmail.com>
2017-12-15 17:46:31 -0500

    started work on bootstrap conversion

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,44 @@ function genNavBar() {
         //document.getElementById("header").innerHTML += "<td>" + "<a href=" + navBarItems[i][0] + " id='navLink'>" + navBarItems[i][1] + "</a></td>";
     }
 }
+
+function genBSNavBar() {
+    var navBarItems = [
+        ["index.html", "Home"],
+        ["about.html", "About"],
+        ["resume.html", "Resume"],
+        ["fizzbuzz.html", "FizzBuzz"]
+    ]
+    var numNavBarItems = navBarItems.length;
+    
+    document.getElementById("navbar").innerHTML += '<nav class="navbar navbar-inverse">'
+    document.getElementById("navbar").innerHTML += '<div class="container-fluid">'
+    document.getElementById("navbar").innerHTML += '<div class="navbar-header"><a class="navbar-brand" href="indexNEW.html">Steven Zamborsky</a></div>'
+    document.getElementById("navbar").innerHTML += '<ul class="nav navbar-nav">'
+    for (var i = 0; i < numNavBarItems; i++) {
+        document.getElementById("navbar").innerHTML += '<li class="active"><a href="'+navBarItems[i][0]+'">'+navBarItems[i][1]+'</a></li>'
+    }
+    document.getElementById("navbar").innerHTML += "</ul>"
+    document.getElementById("navbar").innerHTML += "</div>"
+    document.getElementById("navbar").innerHTML += "</nav>"
+    alert(document.getElementById("navbar").innerHTML)
+}
+
+
+ /*<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header"><a class="navbar-brand" href="indexNEW.html">Steven Zamborsky</a></div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="indexNEW.html">Home</a></li>
+            <li>
+                <a href="about.html">About me</a>
+            </li>
+            <li>
+                <a href="resume.html">Resume</a>
+            </li>
+            <li>
+                <a href="fizzbuzz.html">Fizzbuzz</a>
+            </li>
+        </ul>
+    </div>
+</nav> */
