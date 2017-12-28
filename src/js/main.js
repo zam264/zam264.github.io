4a802612eee17fdce408652cc93d212ae8506ead
Steven Zamborsky <zam264@users.noreply.github.com>
2017-12-27 22:49:11 -0500

    removed un-needed css and added buffers

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,3 @@
-function genNavBar() {
-    var navBarItems = [
-        ["index.html", "Home"],
-        ["about.html", "About"],
-        ["resume.html", "Resume"],
-        ["fizzbuzz.html", "FizzBuzz"]
-    ]
-    var numNavBarItems = navBarItems.length;
-
-    //<button type="button" class="element headerButtons" onclick="location.href='about.html'">About</button>
-
-    for (var i = 0; i < numNavBarItems; i++) {
-        document.getElementById("header").innerHTML += "<button type=\"button\" class=\"element headerButtons\" onclick=\"location.href='" + navBarItems[i][0] + "'\">" + navBarItems[i][1] + "</button>"
-
-
-        //document.getElementById("header").innerHTML += "<td>" + "<a href=" + navBarItems[i][0] + " id='navLink'>" + navBarItems[i][1] + "</a></td>";
-    }
-}
-
 function genBSNavBar() {
     var navBarItems = [
         ["index.html", "Home"],
