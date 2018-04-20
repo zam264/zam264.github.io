2c09a9f7229f19cdf8fc0c110d68e09566c42a20
Steven Zamborsky <zam264@users.noreply.github.com>
2018-04-19 22:41:00 -0400

    Updated crypto page to show gains and losses
    Crypto page now shows gains and losses, updates the style accordingly and shows the difference in parens.  Also fixed the issue where i couldn't do a settimeout with a function accepting params

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
-    getCryptoPrices();
+    var cryptoValues = ["BTC",
+        "ETH",
+        "LTC",
+        "XRP",
+        "BAT",
+        "TRX"];
+    getCryptoPrices(cryptoValues);
 });
 
-function getCryptoPrices() {
-    var cryptoValues = ["BTC", 
-                        "ETH", 
-                        "LTC",
-                        "XRP",
-                        "BAT",
-                        "TRX"];
-    var temp = cryptoValues;
+function getCryptoPrices(cryptoValues) {
     var xhttp = new XMLHttpRequest();
     var url = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptoValues.join()}&tsyms=USD`;
     xhttp.open("POST", url, true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.onreadystatechange = function (aEvt) {
-        if (xhttp.readyState == 4) {
-            if (xhttp.status == 200) {
-                //  alert(xhttp.responseText);
+    xhttp.onload = function (e) {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 200) {
                 var response = JSON.parse(xhttp.responseText);
-                cryptoValues.forEach(updateCryptoPrices.bind(null, response))
+                cryptoValues.forEach(updateCryptoPrices.bind(null, response));
+                setTimeout(function () {
+                    getCryptoPrices(cryptoValues);
+                }, 5000)
+            } else {
+                console.error(xhttp.statusText);
             }
-            else
-                alert("Error loading page\n");
         }
     };
+    xhttp.onerror = function (e) {
+        console.error(xhttp.statusText);
+    };
     xhttp.send();
-    setTimeout(getCryptoPrices, 5000);
 }
 
-
-function updateCryptoPrices(response, item, index){
+function updateCryptoPrices(response, item, index) {
     var responseValue = response[item].USD;
-    document.getElementById(item).innerHTML = `${item} - $${responseValue}`;
-    
-    console.log(`item: ${item} | index: ${index} | response: ${responseValue}`);    
+    var currentDollarAmt = document.getElementById(item).innerHTML.split('$')[1];
+    responseValue = parseFloat(responseValue);
+    currentDollarAmt = parseFloat(currentDollarAmt);
+    var difference = responseValue - currentDollarAmt;
+    difference = Math.round(difference * 100000) / 100000;
+    if (difference < 0) {
+        document.getElementById(item).style.color = "red";
+    }
+    else if (difference > 0) {
+        document.getElementById(item).style.color = "greenyellow";
+    }
+
+    if (document.getElementById(item).innerHTML == "") {
+        document.getElementById(item).innerHTML = `${item} - $${responseValue}`;
+        document.getElementById("refreshTime").innerHTML = "Last refresh: " + new Date().toLocaleString();
+    }
+    else if (difference != 0) {
+        document.getElementById(item).innerHTML = `${item} - $${responseValue} (${difference})`;
+        document.getElementById("refreshTime").innerHTML = "Last refresh: " + new Date().toLocaleString();
+    }
+    console.log(`item: ${item} | index: ${index} | response: ${responseValue}`);
 }
