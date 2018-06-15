c3c0d555e3fca22a1d4064a6a313d9bcf5cc7f4c
Steven Zamborsky <zam264@users.noreply.github.com>
2018-06-14 22:40:51 -0400

    get symbols from user and dynamically update output
    user can now enter a comma delimited list of symbols that.  site will get values and dynamically update output with them.

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,37 @@
 // TODO: Polling to automatically refresh values
-// TODO: Allow user to enter CSV string of values to get
-
-let cryptoValues = ["BTC","ETH","LTC","XRP","BAT","TRX"];
 
 window.onload=function(){
     document.querySelector("#refresh").addEventListener("click", refreshValues)   
+    refreshValues()
 }
 
 let refreshValues = async () => {
-    let prices = await getCryptoPrices(cryptoValues)
+    let symbols = document.getElementById('symbols').value
+    symbols = symbols.split(",")
+
+    let prices = await getCryptoPrices(symbols)
+    // if(prices.Response )
+    prices = await restructureCryptoPrices(prices)
     updateCryptoPrices(prices)
 }
 
 let getCryptoPrices = async (cryptoSymbols) => {
     let url = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptoSymbols.join()}&tsyms=USD`;
     let prices = await fetch(url)
+    if(prices.status != 200){
+        updateWithError("Error: Non 200 response from CryptoCompare")
+        window.stop();
+    }
     prices = await prices.json()
+    if(prices.Response == "Error"){
+        updateWithError("Error: Bad JSON from CryptoCompare")
+        throw new Error()
+    }
     return prices
 }
 
-let updateCryptoPrices = async (prices) => {
+let restructureCryptoPrices = async (prices) => {
+    // Put each crypto into an array
     let pricesArray = []
     Object.keys(prices).forEach( key => {
         let newPrice = {}
@@ -27,36 +39,40 @@ let updateCryptoPrices = async (prices) => {
         pricesArray.push(newPrice)
     })
     prices = pricesArray
-
-    prices.forEach(price => {
-        let symbol = Object.keys(price)[0]
-        let currency = Object.keys(Object.values(price)[0])[0]
-        let value = Object.values(Object.values(price)[0])[0]
-        document.getElementById(symbol).innerHTML = symbol + " - " + currency + " : " + value
+    
+    // Restructure the JSON
+    prices = prices.map(price => {
+        let newPrice = {}
+        newPrice.symbol= Object.keys(price)[0]
+        newPrice.currency = Object.keys(Object.values(price)[0])[0]
+        newPrice.price = Object.values(Object.values(price)[0])[0]
+        return newPrice
     });
+
+    return prices
 }
 
-// function updateCryptoPrices(response, item, index) {
-//     var responseValue = response[item].USD;
-//     var currentDollarAmt = document.getElementById(item).innerHTML.split('$')[1];
-//     responseValue = parseFloat(responseValue);
-//     currentDollarAmt = parseFloat(currentDollarAmt);
-//     var difference = responseValue - currentDollarAmt;
-//     difference = Math.round(difference * 100000) / 100000;
-//     if (difference < 0) {
-//         document.getElementById(item).style.color = "red";
-//     }
-//     else if (difference > 0) {
-//         document.getElementById(item).style.color = "greenyellow";
-//     }
+let updateCryptoPrices = async (prices) => {
+    let cryptoValues = document.getElementById('cryptoValues')
+    cryptoValues.innerHTML = ''
+    
+    prices.forEach(price => {
+        let cryptoDiv = document.createElement('div')
+        cryptoDiv.id = price.symbol
+        cryptoDiv.className = 'crypto-text'
+        cryptoDiv.innerHTML = price.symbol + " - " + price.currency + " : " + price.price
+        cryptoValues.appendChild(cryptoDiv)
+    })
 
-//     if (document.getElementById(item).innerHTML == "") {
-//         document.getElementById(item).innerHTML = `${item} - $${responseValue}`;
-//         document.getElementById("refreshTime").innerHTML = "Last refresh: " + new Date().toLocaleString();
-//     }
-//     else if (difference != 0) {
-//         document.getElementById(item).innerHTML = `${item} - $${responseValue} (${difference})`;
-//         document.getElementById("refreshTime").innerHTML = "Last refresh: " + new Date().toLocaleString();
-//     }
-//     console.log(`item: ${item} | index: ${index} | response: ${responseValue}`);
-// }
+    document.getElementById("refreshTime").innerHTML = "Last refresh: " + new Date().toLocaleString();
+}
+
+let updateWithError = async (error) => {
+    let cryptoValues = document.getElementById('cryptoValues')
+    cryptoValues.innerHTML = ''
+    
+    let cryptoDiv = document.createElement('div')
+    cryptoDiv.className = 'crypto-text'
+    cryptoDiv.innerHTML = error
+    cryptoValues.appendChild(cryptoDiv)
+}
