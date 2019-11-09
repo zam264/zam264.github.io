bc1a33f4926005d735f2539cef9d91a26ede3a73
Steven Zamborsky <zam264@gmail.com>
2019-11-08 22:27:52 -0500

    Initial implementation of home, aboutme and resume pages

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ResumeComponent } from './resume/resume.component';
+
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'aboutme', component: AboutmeComponent},
+  { path: 'resume', component: ResumeComponent}
+
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutmeComponent,
+    ResumeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
