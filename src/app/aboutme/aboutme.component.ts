07079d436eff4024edc2cb5629e2e36a5b135237
Steven Zamborsky <zam264@gmail.com>
2019-11-18 19:56:40 -0500

    Switch to scss

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-aboutme',
   templateUrl: './aboutme.component.html',
-  styleUrls: ['./aboutme.component.css']
+  styleUrls: ['./aboutme.component.scss']
 })
 export class AboutmeComponent implements OnInit {
 
